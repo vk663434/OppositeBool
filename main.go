e200aa105fa246e2ffe795c82b1b7bb1a2520454
vk663434 <78649876+vk663434@users.noreply.github.com>
2023-06-10 17:07:38 +0000

    first init

--- main.go
+++ main.go
@@ -2,22 +2,18 @@ package main
 
 import (
 	"fmt"
-	"math"
-  
+	_ "math"
 )
 
-func squre(number float64) (float64,error){
-  if number < 0 {
-  return 0,fmt.Errorf("invalid number %0.2f",number)
-  }
-  return math.Sqrt(number), nil
+func boolOpposite(myboolen *bool) {
+   *myboolen =! *myboolen 
+  
+  
 }
 
-
-func main() {
-	result,err := squre(-4)
-  if err != nil{
-    fmt.Println(err)
-  }
-  fmt.Println(result)
+func main(){
+  truth := true
+  boolOpposite(&truth)
+  fmt.Println(truth)
+  
 }
